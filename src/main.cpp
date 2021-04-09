d96afe7710ccffb9199a0dd877a0a50b5c9caad9
Chase Ruskin <c.ruskin@ufl.edu>
2021-04-09 19:52:42 -0400

    Implements pan and clear feature

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
 #include <list>
 #include <SFML/Graphics.hpp>
-#include <unordered_set>
 #include <unordered_map>
 #include <utility>
 #include <string>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Whiteboard");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Whiteboard", sf::Style::None);
     std::cout << "Welcome to whiteboard!" << std::endl;
     window.setFramerateLimit(60);
     
     std::unordered_map<std::string, sf::RectangleShape*> pixelLocations;
     sf::Vector2f cord(-100,-100);
+    sf::Vector2f lastCord(-100,-100);
+    sf::Vector2f diff(0,0);
+
     std::string cordKey = "";
     sf::Color penInk = sf::Color::Black;
     int penSize = 6;
 
-    bool pressed = false;
-    bool eraser = false;
+    bool pressed, pan, eraser, cmd;
+    pressed = pan = eraser = cmd = false;
     while(window.isOpen())
     {
+
         sf::Event e;
         while(window.pollEvent(e))
         {
@@ -30,23 +33,54 @@ int main()
             else if(e.type == sf::Event::MouseButtonReleased)
             {
                 pressed = false;
+                pan = false;
             }
             else if(e.type == sf::Event::MouseButtonPressed || pressed)
             {
                 pressed = true;
                 cord = sf::Vector2f(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y);
+                std::cout << cord.x << " " << cord.y << std::endl;
+                if(pan)
+                    diff = cord - lastCord;
+                else
+                    diff = sf::Vector2f(0,0);
+
                 cordKey = std::to_string(int(cord.x)) + "," + std::to_string(int(cord.y));
-                std::cout << cordKey << std::endl;
+                //std::cout << cordKey << std::endl;
+                if(sf::Mouse::isButtonPressed(sf::Mouse::Right))
+                {
+                    pan = true;
+                    //std::cout << diff.x << " " << diff.y << std::endl;
+                }
+                //if(!(cord.x >= 0 && cord.x < window.getSize().x && cord.y >= 0 && cord.y < window.getSize().y))
+                //    pan = false;
+
+                lastCord = cord;
             }
             else if(e.type == sf::Event::KeyPressed)
             {
-                if(e.key.code == sf::Keyboard::G)
+                if(e.key.code == sf::Keyboard::LSystem)
+                {
+                    cmd = true;
+                }
+                else if(e.key.code == sf::Keyboard::Z)
+                {
+                    if(cmd)
+                    {
+                        std::cout << "undo squiggle!" << std::endl;
+                    }
+                }
+                else if(e.key.code == sf::Keyboard::G)
                 {
                     penInk = sf::Color::Green;
                 }
                 else if(e.key.code == sf::Keyboard::B)
                 {
                     penInk = sf::Color::Black;
+                }
+                else if(e.key.code == sf::Keyboard::R)
+                {
+                    penInk = sf::Color::Red;
                 }
                 else if(e.key.code == sf::Keyboard::L)
                 {
@@ -66,19 +100,36 @@ int main()
                 {
                     eraser = !eraser;
                 }
+                else if(e.key.code == sf::Keyboard::C)
+                {
+                    while(pixelLocations.size() > 0)
+                    {
+                        auto it = pixelLocations.begin();
+                        delete it->second;
+                        pixelLocations.erase(it->first);
+                    }
+                }
+            }
+            else if(e.type == sf::Event::KeyReleased)
+            {
+                if(e.key.code == sf::Keyboard::LSystem)
+                {
+                    cmd = false;
+                }
             }
         }
 
-
-        if(pressed && !eraser)
+        if(pressed && !eraser && !pan)
         {
             sf::RectangleShape* temp = new sf::RectangleShape(cord);
             temp->setSize(sf::Vector2f(penSize,penSize));
             temp->setPosition(cord);
             temp->setFillColor(penInk);
             pixelLocations.emplace(cordKey, temp);
+            pixelLocations[cordKey]->setFillColor(temp->getFillColor());
+            //std::cout << "drawn on screen: " << pixelLocations.size() << std::endl;
         }
-        else if(pressed && eraser)
+        else if(pressed && eraser && !pan)
         {
             int eRange = 6;
             for(int i = -eRange; i <= eRange; i++)
@@ -97,6 +148,11 @@ int main()
                 
             }
         }
+        else if(pan)
+        {
+            for(auto it = pixelLocations.begin(); it != pixelLocations.end(); it++)
+                (*it->second).setPosition(it->second->getPosition()+diff);
+        }
 
         window.clear(sf::Color::White);
 
@@ -105,5 +161,12 @@ int main()
             window.draw(*(it->second));
         }
         window.display();
+    }
+
+    while(pixelLocations.size() > 0)
+    {
+        auto it = pixelLocations.begin();
+        delete it->second;
+        pixelLocations.erase(it->first);
     }
 }
