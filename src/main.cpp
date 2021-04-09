535854f828c228a64508a1cbf86bcda271f77e2b
Chase Ruskin <c.ruskin@ufl.edu>
2021-04-08 20:41:07 -0400

    Sets up sfml window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
+    sf::Window window(sf::VideoMode(800, 600), "Whiteboard");
     std::cout << "Welcome to whiteboard!" << std::endl;
+    while(window.isOpen())
+    {
+        sf::Event e;
+        while(window.pollEvent(e))
+        {
+            if(e.type == sf::Event::Closed)
+                window.close();
+        }
+    }
 }
